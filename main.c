f5584ff70ed09c2d2a29c540171973308f6c7009
Dan ELKOUBY <streetwalkermc@gmail.com>
2014-07-03 02:04:03 +0200

    Use defines for constant walues

--- main.c
+++ main.c
@@ -5,16 +5,21 @@
 #include "sprites.h"
 #include "n2DLib.h"
 
+#define GRID_W 10
+#define GRID_H 20
+#define GRID_X 10
+#define GRID_Y 105
+
 void draw_tilemap(const uint8_t map[])
 {
 	int x;
 	int y;
 	uint8_t tile;
-	for (y = 0; y < 20; y++)
-	for (x = 0; x < 10; x++)
+	for (y = 0; y < GRID_H; y++)
+	for (x = 0; x < GRID_W; x++)
 	{
-		tile = map[y * 10 + x];
-		drawSprite(color[tile], x * 11 + 105, y * 11 + 10);
+		tile = map[y * GRID_W + x];
+		drawSprite(color[tile], x * 11 + GRID_Y, y * 11 + GRID_X);
 	}
 }
 
@@ -27,7 +32,7 @@ void piece_draw(int piece, int orientation, int x, int y)
 	{
 		tile = pieces[piece][orientation][i + j * 4];
 		if (tile > 0)
-			drawSprite(color[tile], (x + i) * 11 + 105, (y + j) * 11 + 10);
+			drawSprite(color[tile], (x + i) * 11 + GRID_Y, (y + j) * 11 + GRID_X);
 	}
 }
 
@@ -35,12 +40,12 @@ void piece_merge(int piece, int orientation, int x, int y, uint8_t map[])
 {
 	int i, j;
 	uint8_t tile;
-	for (j = 0; j < 4; j++)
+	for (j = 3; j >= 0; j--)
 	for (i = 0; i < 4; i++)
 	{
 		tile = pieces[piece][orientation][i + j * 4];
 		if (tile > 0)
-			map[(x + i) + (y + j) * 10] = tile;
+			map[(x + i) + (y + j) * GRID_W] = tile;
 	}
 }
 
@@ -54,10 +59,10 @@ int piece_collide(int piece, int orientation, int x, int y, uint8_t map[])
 		tile = pieces[piece][orientation][i + j * 4];
 		if (tile > 0)
 		{
-			if (map[(x + i) + (y + j) * 10])
+			if (map[(x + i) + (y + j) * GRID_W])
 				return 1;
 
-			if ((x + i) < 0 || (x + i) >= 10 || (y + j) >= 20)
+			if ((x + i) < 0 || (x + i) >= GRID_W || (y + j) >= GRID_H)
 				return 1;
 		}
 	}
@@ -68,12 +73,12 @@ int main(void)
 {
 	srand(time(NULL));
 	int i;
-	uint8_t map[200];
-	for (i = 0; i < 200; i++) map[i] = 0;
+	uint8_t map[GRID_H * GRID_W];
+	for (i = 0; i < GRID_H * GRID_W; i++) map[i] = 0;
 	piece_merge(4, 7, 0, 0, map);
 	if (! piece_collide(4, 7, 2, 0, map)) piece_merge(4, 7, 2, 0, map);
 
-	int block_x,block_y, rot, score;
+	//int block_x,block_y, rot, score;
 	initBuffering();
 	clearBufferW();
 
